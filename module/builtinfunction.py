5dad2ceffaf967553e5f50ea76f9e7b906afaa99
김성화 (AI Industry플랫폼개발TF) <seonghwa.kim@kt.com>
2022-01-28 13:49:45 +0900

    python finish

---
+++ module/builtinfunction.py
@@ -0,0 +1,17 @@
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0} 은 아주 좋은 언어입니다. ".format(language))
+
+print(dir())
+import random
+print(dir())
+import pickle
+print(dir())
+
+list = [1, 2, 3]
+print(dir(list))
+
+name = "Jim"
+print(dir(name))
+
+# list of python builtins --> 내장함수 검색법
+# list of python modules --> 외장함수 검색법 
